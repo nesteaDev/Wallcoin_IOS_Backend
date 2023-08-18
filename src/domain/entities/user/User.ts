9a2cd28d41c51484f624042dfedff80c9f592798
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-08-18 01:43:46 -0500

    feat(domain): :recycle: refactor user entity

--- src/domain/entities/user/User.ts
+++ src/domain/entities/user/User.ts
@@ -3,7 +3,6 @@ import Account from '../account/Account';
 export default class User {
   idUser: string;
   name: string;
-  username: string;
   email: string;
   account: Account;
   urlImage: string;
--- src/domain/gateway/user/IUserRepository.ts
+++ src/domain/gateway/user/IUserRepository.ts
@@ -1,10 +1,8 @@
 import CreateUserRequestDto from '../../entities/user/CreateUserRequestDto';
-import AddUserAccountRequestDto from '../../entities/user/AddUserAccountRequestDto';
 import User from '../../entities/user/User';
 
 export default interface IUserRepository {
   createUser(user: CreateUserRequestDto): Promise<User>;
-  addUserAccount(user: AddUserAccountRequestDto): Promise<User>;
   getUserById(idUser: string): Promise<User>;
   getUserByAccountNumber(accountNumber: string): Promise<User>;
   getAllUsers(): Promise<User[]>;
