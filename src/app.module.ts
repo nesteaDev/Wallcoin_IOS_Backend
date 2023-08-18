aada9bd018ed32ee72d12d3c7ea5b807b8372e6e
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-08-18 00:48:42 -0500

    refactor(core): :construction: adding modules for clean architecture
    ading modules domain, infraesturcture, application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DomainModule } from './domain/domain.module';
+import { InfrastructureModule } from './infrastructure/infrastructure.module';
+import { ApplicationModule } from './application/application.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DomainModule, InfrastructureModule, ApplicationModule],
+  controllers: [],
+  providers: [],
+  exports: [],
 })
 export class AppModule {}
--- src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    const name: string = 'Nestea Turner Dev';
-    return `Hello World!, I am ${name}`;
-  }
-}
