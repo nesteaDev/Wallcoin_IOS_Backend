0e052759481108c1948f738391ca7853e3fa431b
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-08-24 13:10:06 -0500

    build(core): :construction: adding script to run microservices and configure ports

--- apps/hub_queue_service/src/main.ts
+++ apps/hub_queue_service/src/main.ts
@@ -3,6 +3,7 @@ import { HubQueueServiceModule } from './hub_queue_service.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(HubQueueServiceModule);
-  await app.listen(3000);
+  await app.listen(3001);
+  console.log(`Server is running on port ${await app.getUrl()}`);
 }
 bootstrap();
