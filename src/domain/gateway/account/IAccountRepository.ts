ebee0cff84a7616917390f3712f15f0b34ed97c9
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-08-24 00:56:32 -0500

    feat(domain): :sparkles: updating methods at gateways

--- src/domain/gateway/account/IAccountRepository.ts
+++ src/domain/gateway/account/IAccountRepository.ts
@@ -1,14 +1,18 @@
-import CreateAccountRequestDto from '../../entities/account/CreateAccountRequestDto';
+import UpdateAccountRequestDto from '../../entities/account/UpdateAccountRequestDto';
 import Account from '../../entities/account/Account';
 import OperationAccountRequestDto from '../../entities/account/OperationAccountRequestDto';
+import OperationAccountResponseDto from '../../entities/account/OperationAccountResponseDto';
 
 export abstract class IAccountRepository {
-  abstract createAccount(account: CreateAccountRequestDto): Promise<Account>;
-  abstract getAccountByUserId(idUser: string): Promise<Account | null>;
+  abstract updateFirstAccount(
+    idAccount: string,
+    dataAccount: UpdateAccountRequestDto,
+  ): Promise<Account>;
+  abstract getAccountByUserId(idUser: string): Promise<Account[] | null>;
   abstract getAccountByAccountNumber(
     accountNumber: string,
   ): Promise<Account | null>;
   abstract updateBalanceAccount(
     data: OperationAccountRequestDto,
-  ): Promise<Account | null>;
+  ): Promise<OperationAccountResponseDto | null>;
 }
